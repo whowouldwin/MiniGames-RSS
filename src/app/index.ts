92b2eab8d48a434f7eca2d7f7833661a24355cf1
whowouldwin <leranetwork@gmail.com>
2026-09-20 06:14:46 +0200

    feat(RSS-QS-1-4-7): add footer navigation and author credits

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,6 +6,8 @@ import { createLeaderboard } from "../components/leaderboard";
 
 import { createGameDevelopment } from "../components/game-dev";
 
+import { createFooter } from "../components/footer";
+
 export const createApp = (): HTMLDivElement => {
   const app: HTMLDivElement = document.createElement("div");
 
@@ -16,6 +18,7 @@ export const createApp = (): HTMLDivElement => {
     createCarousel(),
     createLeaderboard(),
     createGameDevelopment(),
+    createFooter(),
   );
   return app;
 };
